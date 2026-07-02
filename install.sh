58051a0f855ee51d0382f6e3bb515dd66c2d565e
pcpapc172 <apkme11e@gmail.com>
2026-07-02 20:23:56 +0330

    chore: lower min version check to 1.0.0 for pcpapc172 fork

--- install.sh
+++ install.sh
@@ -1352,7 +1352,7 @@ install_x-ui() {
             echo -e "${yellow}Installing the rolling dev build (tag: dev-latest). This is a per-commit pre-release, not a stable version.${plain}"
         else
             tag_version_numeric=${tag_version#v}
-            min_version="2.3.5"
+            min_version="1.0.0"
 
             if [[ "$(printf '%s\n' "$min_version" "$tag_version_numeric" | sort -V | head -n1)" != "$min_version" ]]; then
                 echo -e "${red}Please use a newer version (at least v2.3.5). Exiting installation.${plain}"
